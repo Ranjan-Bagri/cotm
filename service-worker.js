0b55d3d9ce48667cafb202381089b6c02c7b4a79
Ranjan-Bagri <ranjanbagri4119@gmail.com>
2018-04-29 01:53:27 +0530

    updated

--- service-worker.js
+++ service-worker.js
@@ -3,10 +3,18 @@ var cache_name = 'v1';
 var paths = [
   'index.html',
   'style.css',
-  'app.js'
+  'app.js',
+  'manifest.json',
+  'i48.png',
+  'i72.png',
+  'i96.png',
+  'i144.png',
+  'i192.png',
+  'i256.png',
+  'i512.png'
 ];
 
-this.addEventListener( 'install', function ( event ) {
+self.addEventListener( 'install', function ( event ) {
   event.waitUntil( caches.open( cache_name ).then( function ( cache ) {
     return cache.addAll( paths ).then( function () {
       self.skipWaiting();
@@ -14,7 +22,7 @@ this.addEventListener( 'install', function ( event ) {
   } ) );
 }, false );
 
-this.addEventListener( 'fetch', function ( event ) {
+self.addEventListener( 'fetch', function ( event ) {
   event.respondWith( caches.match( event.request ).then( function ( response ) {
     return response || fetch( event.request.clone() ).then( function ( response ) {
       if ( response && response.status === 200 && response.type === 'basic' ) {
@@ -30,7 +38,7 @@ this.addEventListener( 'fetch', function ( event ) {
   } ) );
 }, false );
 
-this.addEventListener( 'activate', function ( event ) {
+self.addEventListener( 'activate', function ( event ) {
   var whitelist = [ cache_name ];
 
   event.waitUntil( caches.keys().then( function ( list ) {
